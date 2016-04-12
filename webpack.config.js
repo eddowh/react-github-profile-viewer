0741919869da0f381c164a0a35f3fbb71d750e53
Eddo Hintoso <eddo.hintoso@gmail.com>
2016-04-12 12:52:38 +0800

    package.json, webpack config, and skeletal dir structure

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: [
+        './src/index.jsx'
+    ],
+    output: {
+        path: __dirname,
+        filename: 'public/js/main.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel',
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
